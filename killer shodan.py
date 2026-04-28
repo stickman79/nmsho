d72ef1113a26462327b08a2776bbe7f938f67943
stickman79 <colabloxyjunin@gmail.com>
2026-04-28 20:14:06 -0300

    Add killer shodan.py to .gitignore
    confirma

--- killer shodan.py
+++ killer shodan.py
@@ -1,22 +1,29 @@
 import subprocess
 import requests
 import sys
+import ip
+import OSINT
+import Sistema, Operacional
+import Portas
 
+print ("KILLER OSINT - shodan+nmap")
+print ("by: xzzyz and slash3r")
 
+SHODAN_API_KEY
 
-def banner()
+def banner():
  print("="*50)
 print ("recon automatizado - SHODAN + NMAP")
 print ("="*50)
 
-def shodan_lookup(ip)
+def shodan_lookup(ip):
       print(f"\n[+] Consultando SHODAN para o ip: {ip}")
-      url =f"https://api.shodan.io/shodan/host/{ip}?key=SHODAN}
+      #   url =f"https://api.shodan.io/shodan/host/{ip}?key=SHODAN}
       response = requests.get(url)
-     data = response.json()
+    data = response.json()
 
      print(f" Organização: {data.get('org', 'N/A')}")
      print(f" - Pais: {data.get('country_name', 'N/A')}"}
      print(f" - Sistema Operacional: {data.get('os', 'N/A')}")
       print(f" - Portas abertas no SHODAN:")
-      for item in data.get("data",[]):
+      for item in data.get("data",[]
