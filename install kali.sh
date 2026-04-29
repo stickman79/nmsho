c907e217f1139ed1eb20c894bc38d4b5859d9f98
stickman79 <colabloxyjunin@gmail.com>
2026-04-29 12:26:43 -0300

    Delete install script for the tool
    Removed installation script for the tool.

--- install kali.sh
+++ install kali.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-echo "Instalando a ferramenta..."
-apt update && apt upgrade -y
-git clone https://github.com/seu-usuario/nome-da-ferramenta.git
-cd nome-da-ferramenta
-pip3 install -r requirements.txt
-python3 tool.py
+
